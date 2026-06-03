7411998f6a6215219a695f35f68220a8bba45516
Willian Pramio <willianpramio@hotmail.com>
2026-06-03 11:00:13 -0300

    chore: remove unused JS files

--- src/symptoms_analyser/static/js/admin_patients.js
+++
@@ -1,3 +0,0 @@
-// This file is purposely left empty because the Patient Isolation mapping
-// dashboard uses 100% native server-side rendering (SSR), Flask sessions,
-// and message flashing for a secure, zero-Javascript footprint!
